3cf96c8971b88622a4001962b10960ba015844db
getNick <aleksandr.kostiuk687@gmail.com>
2017-09-26 22:54:48 +0300

    init js folder,add jquery,paper, draw in canvas, onMouseDown event

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById("mainCanvas"));
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = "green";
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = "white";
+    text.fontSize = 20;
+    text.content = "hello world";
+
+    var tool = new Tool();
+    tool.onMouseDown = function (event) {
+        var c = Shape.Circle(event.point, 40);
+        c.fillColor = 'red';
+    }
+    paper.view.draw();
+});
