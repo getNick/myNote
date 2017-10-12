e5227d92fff128df562aa3eaa314587819fc6291
getNick <aleksandr.kostiuk687@gmail.com>
2017-10-12 23:46:35 +0300

    button click

--- js/public/dist/script.js
+++ js/public/dist/script.js
@@ -1,10 +1,11 @@
 "use strict";
 
-document.ready(function () {
+$(document).ready(function () {
 	"use strict";
 
 	paper.install(window);
-	paper.setup(document.getElementById("mainCanvas"));
+	var convas = document.getElementById("mainCanvas");
+	paper.setup(convas);
 	var c = Shape.Circle(200, 200, 80);
 	c.fillColor = "green";
 	var text = new PointText(200, 200);
@@ -19,4 +20,18 @@ document.ready(function () {
 		c.fillColor = "red";
 	};
 	paper.view.draw();
+	var button = document.getElementById("btn");
+	button.innerHTML = "Показать зайку";
+	button.onclick = function (event) {
+		var name = prompt("Введите имя");
+		var myImage = document.getElementById('image');
+		var mySrc = myImage.getAttribute('src');
+		if (mySrc === "") {
+			myImage.setAttribute("src", "content/photo_2017-09-25_17-50-44.jpg");
+			button.innerHTML = "\u0421\u043F\u0440\u044F\u0442\u0430\u0442\u044C " + name;
+		} else {
+			myImage.setAttribute("src", "");
+			button.innerHTML = "\u041F\u043E\u043A\u0430\u0437\u0430\u0442\u044C " + name;
+		}
+	};
 });
--- js/public/es6/script.js
+++ js/public/es6/script.js
@@ -1,7 +1,8 @@
-document.ready(function () {
+$(document).ready(function () {
 	"use strict";
 	paper.install(window);
-	paper.setup(document.getElementById("mainCanvas"));
+	var convas = document.getElementById("mainCanvas");
+	paper.setup(convas);
 	var c = Shape.Circle(200, 200, 80);
 	c.fillColor = "green";
 	var text = new PointText(200, 200);
@@ -9,11 +10,25 @@ document.ready(function () {
 	text.fillColor = "white";
 	text.fontSize = 20;
 	text.content = "hello world";
-
+	
 	var tool = new Tool();
 	tool.onMouseDown = function (event) {
 		var c = Shape.Circle(event.point, 40);
 		c.fillColor = "red";
 	};
 	paper.view.draw();
+	var button = document.getElementById("btn");
+	button.innerHTML="Показать зайку";
+	button.onclick = function (event) {
+		var name = prompt("Введите имя");
+		var myImage = document.getElementById('image');
+		var mySrc = myImage.getAttribute('src');
+		if (mySrc === "") {
+			myImage.setAttribute("src", "content/photo_2017-09-25_17-50-44.jpg");
+			button.innerHTML = `Спрятать ${name}`;
+		} else {
+			myImage.setAttribute("src", "");
+			button.innerHTML = `Показать ${name}`;
+		}
+	}
 });
